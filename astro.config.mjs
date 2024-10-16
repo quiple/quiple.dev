88f963e792ed06926ade2e84389225c6b2a9bdfa
Minseo Lee <itoupluk427@gmail.com>
2024-10-16 15:54:27 +0900

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  })]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 })
