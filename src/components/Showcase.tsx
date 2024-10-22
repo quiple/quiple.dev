f8e941a0925ac6e88bf2d9d397f808c3103f2174
Minseo Lee <itoupluk427@gmail.com>
2024-10-22 21:37:36 +0900

    Update Showcase.tsx

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,3 +1,4 @@
+import { getImage } from 'astro:assets'
 import sc200223 from '@/assets/showcase/200223.png'
 import sc200622 from '@/assets/showcase/200622.png'
 import sc201221 from '@/assets/showcase/201221.png'
@@ -25,14 +26,16 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel'
+import type { GetImageResult } from 'astro'
 import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
+const format = 'avif'
 
 interface game {
   title: string
   author: string
   link: string
   type: 'steam' | 'appstore' | 'patch'
-  image: ImageMetadata
+  image: GetImageResult
 }
 
 const games: game[] = [
@@ -41,140 +44,140 @@ const games: game[] = [
     author: 'Moonana',
     link: '1325040',
     type: 'steam',
-    image: sc240919,
+    image: await getImage({ src: sc240919, format: format }),
   },
   {
     title: '소년기의 끝',
     author: 'buriki clock',
     link: '2403290',
     type: 'steam',
-    image: sc240905,
+    image: await getImage({ src: sc240905, format: format }),
   },
   {
     title: 'Time Treker',
     author: 'Fuse Game',
     link: '2776500',
     type: 'steam',
-    image: sc240710,
+    image: await getImage({ src: sc240710, format: format }),
   },
   {
     title: 'ZeroRanger',
     author: 'Robeureu',
     link: 'blog.naver.com/robeureu/223357336308',
     type: 'patch',
-    image: sc240218,
+    image: await getImage({ src: sc240218, format: format }),
   },
   {
     title: 'STONKS-9800',
     author: 'TERNOX',
     link: '1539140',
     type: 'steam',
-    image: sc240202,
+    image: await getImage({ src: sc240202, format: format }),
   },
   {
     title: '미제사건은 끝내야 하니까',
     author: 'Somi',
     link: '2676840',
     type: 'steam',
-    image: sc240118,
+    image: await getImage({ src: sc240118, format: format }),
   },
   {
     title: 'VVVVVV',
     author: 'Terry Cavanagh',
     link: '70300',
     type: 'steam',
-    image: sc240111_2,
+    image: await getImage({ src: sc240111_2, format: format }),
   },
   {
     title: 'Momodora: Moonlit Farewell',
     author: 'Bombservice',
     link: '1747760',
     type: 'steam',
-    image: sc240111,
+    image: await getImage({ src: sc240111, format: format }),
   },
   {
     title: 'Coffee Talk Episode 2: Hibiscus & Butterfly',
     author: 'Toge Productions',
     link: '1663220',
     type: 'steam',
-    image: sc230420,
+    image: await getImage({ src: sc230420, format: format }),
   },
   {
     title: 'Papers, Please',
     author: 'Lucas Pope',
     link: '239030',
     type: 'steam',
-    image: sc221127,
+    image: await getImage({ src: sc221127, format: format }),
   },
   {
     title: 'Dungeon Squad',
     author: 'GameCoaster',
     link: '1642733080',
     type: 'appstore',
-    image: sc221006,
+    image: await getImage({ src: sc221006, format: format }),
   },
   {
     title: 'Shotgun King: The Final Checkmate',
     author: 'PUNKCAKE Delicieux',
     link: '1972440',
     type: 'steam',
-    image: sc220621,
+    image: await getImage({ src: sc220621, format: format }),
   },
   {
     title: 'Monster Sanctuary',
     author: '솔라리어스',
     link: 'blog.naver.com/ansewo/222702695752',
     type: 'patch',
-    image: sc220507,
+    image: await getImage({ src: sc220507, format: format }),
   },
   {
     title: 'NEEDY GIRL OVERDOSE',
     author: '코스믹딜루즈 & quiple',
     link: 'cosmicdeluge.tistory.com/5',
     type: 'patch',
-    image: sc220220,
+    image: await getImage({ src: sc220220, format: format }),
   },
   {
     title: 'Teamfight Manager',
     author: 'Team Samoyed',
     link: '1372810',
     type: 'steam',
-    image: sc210302,
+    image: await getImage({ src: sc210302, format: format }),
   },
   {
     title: 'BLUE REVOLVER',
     author: 'Sepheille',
     link: 'steamcommunity.com/sharedfiles/filedetails/?id=2405396574',
     type: 'patch',
-    image: sc210223,
+    image: await getImage({ src: sc210223, format: format }),
   },
   {
     title: 'Duke Dashington Remastered',
     author: '바람 번역단',
     link: 'blog.naver.com/bleach1491/222196942354',
     type: 'patch',
-    image: sc210105,
+    image: await getImage({ src: sc210105, format: format }),
   },
   {
     title: 'Rev',
     author: '바람 번역단',
     link: 'blog.naver.com/bleach1491/222180900832',
     type: 'patch',
-    image: sc201221,
+    image: await getImage({ src: sc201221, format: format }),
   },
   {
     title: 'Alwa’s Legacy',
     author: '바람 번역단',
     link: 'blog.naver.com/bleach1491/222008221688',
     type: 'patch',
-    image: sc200622,
+    image: await getImage({ src: sc200622, format: format }),
   },
   {
     title: 'Westerado: Double Barreled',
     author: '바람 번역단',
     link: 'blog.naver.com/bleach1491/221820971383',
     type: 'patch',
-    image: sc200223,
+    image: await getImage({ src: sc200223, format: format }),
   },
 ]
 
