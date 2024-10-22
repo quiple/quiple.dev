c3e399af59c6908e711370e5f1ed9b288001ff1f
Minseo Lee <itoupluk427@gmail.com>
2024-10-22 11:55:49 +0900

    Create Downloads.tsx

---
+++ src/components/Downloads.tsx
@@ -0,0 +1,15 @@
+const fonts = ['14', '11', '11 Bold', '11 Condensed', '9', '7', 'Mono11', 'Mono9', 'Mono7']
+
+export function Downloads() {
+  return (
+    <div>
+      {fonts.map((font) => {
+        return (
+          <a key={font} href={`https://cdn.jsdelivr.net/npm/galmuri/dist/${font}.ttf`}>
+            Galmuri{font}
+          </a>
+        )
+      })}
+    </div>
+  )
+}
