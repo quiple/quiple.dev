cf9b1ecb3314d3b814f8a297d185f7b98da7f220
Minseo Lee <itoupluk427@gmail.com>
2024-10-23 13:48:43 +0900

    tester

--- src/components/galmuri/Downloads.tsx
+++ src/components/galmuri/Downloads.tsx
@@ -1,11 +1,15 @@
-const fonts = ['14', '11', '11 Bold', '11 Condensed', '9', '7', 'Mono11', 'Mono9', 'Mono7']
+import { fonts } from '@/lib/utils'
 
 export function Downloads() {
   return (
     <div>
       {fonts.map((font) => {
         return (
-          <a key={font} href={`https://cdn.jsdelivr.net/npm/galmuri/dist/${font}.ttf`} download>
+          <a
+            key={font}
+            href={`https://cdn.jsdelivr.net/npm/galmuri/dist/${font.replaceAll(' ', '-')}.ttf`}
+            download
+          >
             Galmuri{font}
           </a>
         )
---
+++ src/components/galmuri/Tester.tsx
@@ -0,0 +1,26 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Textarea } from '@/components/ui/textarea'
+
+export function Tester() {
+  return (
+    <>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+      <Textarea />
+    </>
+  )
+}
