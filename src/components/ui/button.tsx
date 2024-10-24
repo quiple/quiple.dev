a832cd6f8cb6e392d0cfb0a79dc493c6bd5e47ef
Minseo Lee <itoupluk427@gmail.com>
2024-10-24 15:12:50 +0900

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        galmuri: 'rounded-none !pl-3 !pr-[calc(.75rem_-_var(--px))]',
+        galmuri: 'rounded-none !pl-3 !pr-[calc(.75rem_-_var(--px))] focus-visible:ring-[length:var(--px)] focus-visible:ring-[] focus-visible:ring-offset-[length:var(--px)]',
         galmuriSecondary: 'rounded-none',
       },
       size: {
