f233ececa853e65d6941f72818b0cd20d0d7662a
Minseo Lee <itoupluk427@gmail.com>
2024-10-28 13:24:22 +0900

    Update ModeToggle.tsx

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -31,7 +31,7 @@ export function ModeToggle() {
         <Button variant="ghost" size="icon" className="button icon-button w-8 h-8">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">테마 변경</span>
+          <span className="sr-only">{m.theme_toggle()}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
