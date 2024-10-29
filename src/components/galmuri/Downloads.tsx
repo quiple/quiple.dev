0a2a32e73afda702019f8328ed9d92b38a0b64c2
Minseo Lee <itoupluk427@gmail.com>
2024-10-29 16:42:40 +0900

    Update Downloads.tsx

--- src/components/galmuri/Downloads.tsx
+++ src/components/galmuri/Downloads.tsx
@@ -1,6 +1,8 @@
 import { fonts } from '@/components/galmuri/data'
 import { buttonVariants } from '@/components/ui/button'
 
+const families = Array.from(new Set(fonts.map((font) => font.family)))
+
 export function Downloads() {
   return (
     <div className="download">
@@ -11,8 +13,11 @@ export function Downloads() {
             key={font.slug}
             href={`https://cdn.jsdelivr.net/npm/galmuri/dist/${font.name.replaceAll(' ', '-')}.ttf`}
             download
+            data-umami-event="Galmuri 다운로드"
+            data-umami-event-font={font.name}
+            data-umami-event-format="TTF"
           >
-            {font.name}
+            {font.name}.ttf
           </a>
         )
       })}
