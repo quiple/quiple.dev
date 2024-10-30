688d9bf19d3271909fe5fbb4641901f0272a6c68
Minseo Lee <itoupluk427@gmail.com>
2024-10-30 15:24:37 +0900

    card

--- src/components/galmuri/Downloads.tsx
+++ src/components/galmuri/Downloads.tsx
@@ -1,24 +1,40 @@
-import { fonts } from '@/components/galmuri/data'
+import { fonts, formats } from '@/components/galmuri/data'
 import { buttonVariants } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 const families = Array.from(new Set(fonts.map((font) => font.family)))
 
 export function Downloads() {
   return (
     <div className="download">
-      {fonts.map((font) => {
+      {families.map((familyName) => {
         return (
-          <a
-            className={`${buttonVariants({ variant: 'outline' })} button outline-button`}
-            key={font.slug}
-            href={`https://cdn.jsdelivr.net/npm/galmuri/dist/${font.name.replaceAll(' ', '-')}.ttf`}
-            download
-            data-umami-event="Galmuri 다운로드"
-            data-umami-event-font={font.name}
-            data-umami-event-format="TTF"
-          >
-            {font.name}.ttf
-          </a>
+          <Card key={familyName} className="card">
+            <CardHeader>
+              <CardTitle>{familyName}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              {fonts
+                .filter((font) => font.family === familyName)
+                .map((font) => {
+                  return formats.map((format) => {
+                    return (
+                      <a
+                        key={font.slug}
+                        className={`${buttonVariants({ variant: 'outline' })} button outline-button`}
+                        href={`https://cdn.jsdelivr.net/npm/galmuri/dist/${font.name.replaceAll(' ', '-')}.${format}`}
+                        download
+                        data-umami-event="Galmuri 다운로드"
+                        data-umami-event-font={font.name}
+                        data-umami-event-format={format}
+                      >
+                        {font.style} {format}
+                      </a>
+                    )
+                  })
+                })}
+            </CardContent>
+          </Card>
         )
       })}
     </div>
