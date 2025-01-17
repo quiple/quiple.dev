45eba72f9b65d32a6a9fcb8b63d12509d13c7699
Minseo Lee <itoupluk427@gmail.com>
2025-01-17 19:23:10 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
   ],
   vite: {
     resolve: {
+      // @ts-ignore
       alias: import.meta.env.PROD && {
         'react-dom/server': 'react-dom/server.edge',
       },
