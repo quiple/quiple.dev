f98e34e8715d82431ced8f4362d89fcfc2edb0cb
Minseo Lee <itoupluk427@gmail.com>
2025-02-17 16:54:01 +0900

    env 테스트

---
+++ env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="astro/client" />
+
+type ENV = {
+  chzzk: {
+    clientId: string
+    redirectUri: string
+    state: string
+  }
+}
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>
+declare namespace App {
+  interface Locals extends Runtime {}
+}
