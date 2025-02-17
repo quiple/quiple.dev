4ac1358eea82f53a8ef25ac9512c92d5abc45d2a
Minseo Lee <itoupluk427@gmail.com>
2025-02-17 16:54:31 +0900

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -8,7 +8,6 @@ type ENV = {
   }
 }
 
-// use a default runtime configuration (advanced mode).
 type Runtime = import('@astrojs/cloudflare').Runtime<ENV>
 declare namespace App {
   interface Locals extends Runtime {}
