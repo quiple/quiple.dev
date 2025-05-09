1726ea9defbd68544523e8289b76e04dd5c2c337
Minseo Lee <itoupluk427@gmail.com>
2025-05-09 10:23:19 +0900

    Update galmuri.js

--- schemas/galmuri.js
+++ schemas/galmuri.js
@@ -7,11 +7,13 @@ export default {
       name: 'title',
       title: '제목',
       type: 'string',
+      validation: (rule) => rule.required(),
     },
     {
       name: 'showcase',
       title: '쇼케이스',
       type: 'array',
+      validation: (rule) => rule.required(),
       of: [{type: 'game'}],
     },
   ],
