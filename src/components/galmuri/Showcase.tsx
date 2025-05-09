0041df0b963c746d63965d86fbbba64f11842b27
Minseo Lee <itoupluk427@gmail.com>
2025-05-09 13:04:02 +0900

    type 오류 해결

--- src/components/galmuri/Showcase.tsx
+++ src/components/galmuri/Showcase.tsx
@@ -11,6 +11,7 @@ import {loadQuery} from '@/lib/sanity'
 import type {SanityDocument} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 import {WheelGesturesPlugin} from 'embla-carousel-wheel-gestures'
+import type {Game} from 'sanity.types'
 
 const {data: galmuri} = await loadQuery<SanityDocument[]>({
   query: `*[_type == "galmuri"]`,
@@ -32,38 +33,40 @@ export function Showcase() {
       plugins={[WheelGesturesPlugin()]}
     >
       <CarouselContent className="-ml-[1em]">
-        {galmuri[0].showcase.map((game) => {
-          const size = game.screenshot.asset._ref.split('-')[2]
-          const aspectRatio = size.split('x')[0] / size.split('x')[1]
-          const image = builder
-            .image(game.screenshot.asset._ref)
-            .height(640)
-            .fit('max')
-            .auto('format')
-          console.log(game.screenshot.asset._ref.split('-')[2])
-          return (
-            <CarouselItem key={game.title} className="basis-auto pl-[1em]">
-              <figure>
-                <Skeleton
-                  style={{aspectRatio: aspectRatio}}
-                  className="absolute -z-10 rounded-none h-40 sm:h-52 md:h-60 lg:h-72 xl:h-80"
-                />
-                <img style={{aspectRatio: aspectRatio}} src={image.url()} alt={game.title} />
-                <span className="overlay" />
-                <figcaption>
-                  <a
-                    href={linkPrefix[game.type] + game.url}
-                    target="_blank"
-                    rel="nofollow noreferrer noopener"
-                  >
-                    {game.title}
-                  </a>{' '}
-                  {game.type === 'patch' ? 'by' : '©'} {game.author}
-                  {game.type === 'patch' && ' (사용자 패치)'}
-                </figcaption>
-              </figure>
-            </CarouselItem>
-          )
+        {galmuri[0].showcase.reverse().map((game: Game) => {
+          if (game.screenshot?.asset && game.type) {
+            const size = game.screenshot.asset._ref.split('-')[2]
+            const aspectRatio = Number(size.split('x')[0]) / Number(size.split('x')[1])
+            const image = builder
+              .image(game.screenshot.asset._ref)
+              .height(640)
+              .fit('max')
+              .auto('format')
+
+            return (
+              <CarouselItem key={game.title} className="basis-auto pl-[1em]">
+                <figure>
+                  <Skeleton
+                    style={{aspectRatio: aspectRatio}}
+                    className="absolute -z-10 rounded-none h-40 sm:h-52 md:h-60 lg:h-72 xl:h-80"
+                  />
+                  <img style={{aspectRatio: aspectRatio}} src={image.url()} alt={game.title} />
+                  <span className="overlay" />
+                  <figcaption>
+                    <a
+                      href={linkPrefix[game.type] + game.url}
+                      target="_blank"
+                      rel="nofollow noreferrer noopener"
+                    >
+                      {game.title}
+                    </a>{' '}
+                    {game.type === 'patch' ? 'by' : '©'} {game.author}
+                    {game.type === 'patch' && ' (사용자 패치)'}
+                  </figcaption>
+                </figure>
+              </CarouselItem>
+            )
+          }
         })}
       </CarouselContent>
       <CarouselPrevious />
