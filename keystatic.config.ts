804574af3b72f3b2866df67593cffe77c974907e
Minseo Lee <itoupluk427@gmail.com>
2025-05-11 03:06:23 +0900

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -24,13 +24,14 @@ export default config({
           fields.object({
             title: fields.text({label: '제목', validation: {isRequired: true}}),
             author: fields.text({label: '저작권자 또는 제작자', validation: {isRequired: true}}),
-            type: fields.select({label: '유형',
+            type: fields.select({
+              label: '유형',
               options: [
-                { label: 'Steam', value: 'steam' },
-                { label: 'App Store', value: 'appstore' },
-                { label: '사용자 패치', value: 'patch' },
+                {label: 'Steam', value: 'steam'},
+                {label: 'App Store', value: 'appstore'},
+                {label: '사용자 패치', value: 'patch'},
               ],
-              defaultValue: 'steam'
+              defaultValue: 'steam',
             }),
             link: fields.text({label: '링크'}),
             screenshot: fields.image({label: '스크린샷', validation: {isRequired: true}}),
