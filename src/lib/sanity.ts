ada5ddee68c3a381f3df33dddc4be169217fe1fe
Minseo Lee <itoupluk427@gmail.com>
2025-05-10 23:25:40 +0900

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,6 +4,6 @@ import type {SanityAsset} from '@sanity/image-url/lib/types/types'
 
 export const imageBuilder = imageUrlBuilder(sanityClient)
 
-export function urlForImage(source: SanityAsset) {
+export const urlForImage = (source: SanityAsset) => {
   return imageBuilder.image(source)
 }
