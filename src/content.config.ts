b2da9c0f35f4e74ca6c49f864e8e842af478b51e
Minseo Lee <itoupluk427@gmail.com>
2025-05-23 12:53:38 +0900

    blocks

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,10 +3,8 @@ import {file} from 'astro/loaders'
 import {parse} from 'csv-parse/sync'
 
 const unicodeBlocks = defineCollection({
-  loader: file('src/contents/unicode-blocks.yaml'),
-  schema: z.object({
-    last: z.string(),
-    name: z.string(),
+  loader: file('src/contents/unicode-blocks.csv', {
+    parser: (text) => parse(text, {delimiter: ';', columns: true, skipEmptyLines: true}),
   }),
 })
 
