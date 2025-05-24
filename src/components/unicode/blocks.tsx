911378613d182f700a5c8e691321ee569250f313
Minseo Lee <itoupluk427@gmail.com>
2025-05-24 18:31:00 +0900

    update

--- src/components/unicode/blocks.tsx
+++ src/components/unicode/blocks.tsx
@@ -3,7 +3,7 @@ import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '@/c
 export function BlocksTable({unicodeBlocks}: {unicodeBlocks: any}) {
   return (
     <Table>
-      <TableHeader>
+      <TableHeader className="pointer-events-none">
         <TableRow>
           <TableHead>범위</TableHead>
           <TableHead>이름</TableHead>
@@ -11,9 +11,15 @@ export function BlocksTable({unicodeBlocks}: {unicodeBlocks: any}) {
       </TableHeader>
       <TableBody>
         {unicodeBlocks.map((block: any) => (
-          <TableRow key={block.data.id}>
+          <TableRow
+            key={block.id}
+            className="cursor-pointer"
+            onClick={() => {
+              location.href = `/unicode/block/${block.id}`
+            }}
+          >
             <TableCell>
-              {block.data.id}~{block.data.last}
+              {block.id}~{block.data.last}
             </TableCell>
             <TableCell>
               {block.data.nameKo}
