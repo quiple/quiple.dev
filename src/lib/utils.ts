a4d6c99b73ce8dc9dc2426021c44de5776a42da5
Minseo Lee <itoupluk427@gmail.com>
2025-05-25 23:21:29 +0900

    대문자

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import {type ClassValue, clsx} from 'clsx'
 import {twMerge} from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export const unicodeTitleCase = (e: string) =>
+  e.replaceAll('Cjk', 'CJK').replace(/-[0-9A-Fa-f]{4}$/, (e) => e.toUpperCase())
