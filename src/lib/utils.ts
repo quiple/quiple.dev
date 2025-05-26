676d314dd58cd43c908c0648aa77c246e68139e9
Minseo Lee <itoupluk427@gmail.com>
2025-05-26 17:36:23 +0900

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,4 +4,7 @@ import {twMerge} from 'tailwind-merge'
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const unicodeTitleCase = (e: string) =>
-  e.replaceAll(/(?<=\W*)Cjk(?=\W*)/g, 'CJK').replaceAll(/(?<=\W*)up(?=\W*)/g, 'Up').replace(/-[0-9A-Fa-f]{4,5}$/, (e) => e.toUpperCase())
+  e
+    .replaceAll(/(?<=\W*)Cjk(?=\W*)/g, 'CJK')
+    .replaceAll(/(?<=\W*)up(?=\W*)/g, 'Up')
+    .replace(/-[0-9A-Fa-f]{4,5}$/, (e) => e.toUpperCase())
