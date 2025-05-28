098240c91d1321737028b0f4902cb42ec74098b9
Minseo Lee <itoupluk427@gmail.com>
2025-05-28 17:12:08 +0900

    Update blocks.tsx

--- src/components/unicode/blocks.tsx
+++ src/components/unicode/blocks.tsx
@@ -30,7 +30,7 @@ export function BlocksTable({blocks, data}: {blocks: UnicodeBlock[]; data: Unico
           return ['private', 'surrogate'].some((i) => block.name.toLowerCase().includes(i)) ? (
             <TableRow key={block.first} className="pointer-events-none">
               <TableCell>
-                U+{block.first}~{block.last}
+                U+{block.first.toString(16).padStart(4, '0').toUpperCase()}~{block.last.toString(16).padStart(4, '0').toUpperCase()}
               </TableCell>
               <TableCell>
                 {block.nameKo}
@@ -47,7 +47,7 @@ export function BlocksTable({blocks, data}: {blocks: UnicodeBlock[]; data: Unico
               }}
             >
               <TableCell className="tabular-nums">
-                U+{block.first}~{block.last}
+                U+{block.first.toString(16).padStart(4, '0').toUpperCase()}~{block.last.toString(16).padStart(4, '0').toUpperCase()}
               </TableCell>
               <TableCell>
                 {block.nameKo}
