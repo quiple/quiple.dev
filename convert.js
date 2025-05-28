2aa554acc421c3ed2b02595f52732461b84514d9
Minseo Lee <itoupluk427@gmail.com>
2025-05-28 16:44:01 +0900

    csv

---
+++ convert.js
@@ -0,0 +1,28 @@
+import fs from 'node:fs'
+import {parse} from 'csv-parse/sync'
+
+const data = parse(await fs.promises.readFile(`${process.cwd()}/unicode_data.csv`, 'utf8'), {
+  delimiter: ';',
+}).map((char, i) => {
+  if (i !== 0) {
+    char[0] = parseInt(char[0], 16)
+    char[12] = char[12] && parseInt(char[12], 16)
+    char[13] = char[13] && parseInt(char[13], 16)
+    char[14] = char[14] && parseInt(char[14], 16)
+  }
+  return char.join(';')
+})
+
+await fs.promises.writeFile(`${process.cwd()}/unicode_data.output.csv`, data.join('\n'), 'utf8')
+
+const blocks = parse(await fs.promises.readFile(`${process.cwd()}/unicode_blocks.csv`, 'utf8'), {
+  delimiter: ';',
+}).map((char, i) => {
+  if (i !== 0) {
+    char[0] = parseInt(char[0], 16)
+    char[1] = parseInt(char[1], 16)
+  }
+  return char.join(';')
+})
+
+await fs.promises.writeFile(`${process.cwd()}/unicode_blocks.output.csv`, blocks.join('\n'), 'utf8')
