ad63b9de4e0b11b49b2bf0d1057683fd7b2a0fd9
Minseo Lee <itoupluk427@gmail.com>
2025-06-02 14:12:34 +0900

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,6 @@ export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const unicodeTitleCase = (e: string) =>
   e
-    .replaceAll(/(?<=\W)Cjk(?=\W)/g, 'CJK')
-    .replaceAll(/(?<=\W)up(?=\W)/g, 'Up')
+    .replaceAll(/\bCjk\b/g, 'CJK')
+    .replaceAll(/\bup\b/g, 'Up')
     .replace(/-[0-9A-Fa-f]{4,5}$/, (e) => e.toUpperCase())
