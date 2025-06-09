9ddce2cd301f3cc32597a6410407f1f30bda1646
Minseo Lee <itoupluk427@gmail.com>
2025-06-09 23:39:30 +0900

    unicode

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,3 @@ import {type ClassValue, clsx} from 'clsx'
 import {twMerge} from 'tailwind-merge'
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
-
-export const unicodeTitleCase = (e: string) =>
-  e
-    .replaceAll(/\bCjk\b/g, 'CJK')
-    .replaceAll(/\bup\b/g, 'Up')
-    .replace(/-[0-9A-Fa-f]{4,5}$/, (e) => e.toUpperCase())
